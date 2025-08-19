3a0669f7434a7826d1f36fc30894a6f597542368
Dominique Theodore <dominique.theodore86@gmail.com>
2025-08-19 13:53:49 +0400

    recalculate totals and exempt income

--- hrms/payroll/doctype/statement_of_emoluments/statement_of_emoluments.py
+++ hrms/payroll/doctype/statement_of_emoluments/statement_of_emoluments.py
@@ -3,7 +3,26 @@
 
 # import frappe
 from frappe.model.document import Document
-
+from frappe.utils import flt
 
 class StatementofEmoluments(Document):
-	pass
+	def validate(self):
+		self.recalculate_total()
+        
+	def recalculate_total(self):
+		self.total_emoluments = (
+				flt(self.salary_wages_basic) +
+				flt(self.bonus_including_end_of_year) +
+				flt(self.rent_or_housing_allowance) +
+				flt(self.entertainment_allowance) +
+				flt(self.transport_allowance) +
+				flt(self.reimbursement_travelling_expenses) +
+				flt(self.other_allowance) +
+				flt(self.reimbursement_personal_expenses) +
+				flt(self.reimbursement_passages) +
+				flt(self.fringe_benefits) +
+				flt(self.lump_sum_commutation) +
+				flt(self.retirement_pension)
+			)
+		self.emoluments_net_of_exempt_income = self.total_emoluments - self.exempt_income
+
